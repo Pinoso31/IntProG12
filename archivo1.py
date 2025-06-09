bd468ff3a1ecbff259c3c8b484b7ca06ae149025
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-06-09 11:50:59 -0600

    primer commit

---
+++ archivo1.py
@@ -0,0 +1,7 @@
+try:
+    mi_archivo = open("C:\\texto_file.txt", "r")
+    contenido = mi_archivo.read()
+    print(contenido)
+    mi_archivo.close()
+except FileNotFoundError:
+    print("El archivo no se encuentra")
