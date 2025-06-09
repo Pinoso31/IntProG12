f1ecbc8e5815b0fa40d6b222ac6186b3864bd739
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-06-09 12:38:40 -0600

    archivos agregados

---
+++ archivo3.py
@@ -0,0 +1,22 @@
+#sumar dos numeros 
+import os
+def sumar(a, b):
+    return a + b
+
+def generar_archivo(nombre_archivo, contenido):
+    with open(nombre_archivo, "w") as archivo:
+        archivo.write(contenido)
+
+def main():
+    a = int(input("Ingrese el primer numero : "))
+    b = int(input("Ingrese el segundo numero : "))
+    resultado = sumar(a, b)
+
+    contenido = f"La suma de {a} y {b} es : {resultado}"
+    print(contenido)
+
+    generar_archivo("resultado_suma.txt", contenido)
+    print("El resultado se ha guardado en ´resultado_suma.txt´")
+
+main()
+os.sytem("notepad.exe")
